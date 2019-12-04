f6aa3271e77cd2929b03794a9c312aa9b23b3eb0
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-12-04 21:43:43 +0200

    replay fixes

--- handlers/getFullReplayHandler.py
+++ handlers/getFullReplayHandler.py
@@ -1,9 +1,11 @@
 import tornado.gen
 import tornado.web
-
+from objects import glob
 from common.web import requestsManager
 from constants import exceptions
 from helpers import replayHelper
+from helpers import replayHelperAuto
+from helpers import replayHelperRelax
 from common.sentry import sentry
 
 MODULE_NAME = "get_full_replay"
@@ -16,7 +18,19 @@ class handler(requestsManager.asyncRequestHandler):
 	@sentry.captureTornado
 	def asyncGet(self, replayID):
 		try:
-			fullReplay = replayHelper.buildFullReplay(scoreID=replayID)
+			#try to found vanilla replay
+			
+			try:
+				fullReplay = replayHelper.buildFullReplay(scoreID=replayID)
+			except (exceptions.fileNotFoundException, exceptions.scoreNotFoundError):
+				#try found relax replay (yes, try, try, try)
+				try:
+					fullReplay = replayHelperRelax.buildFullReplay(scoreID=replayID)
+				except (exceptions.fileNotFoundException, exceptions.scoreNotFoundError):
+					try:
+						fullReplay = replayHelperAuto.buildFullReplay(scoreID=replayID)
+					except (exceptions.fileNotFoundException, exceptions.scoreNotFoundError):
+						raise exceptions.fileNotFoundException(MODULE_NAME, None)
 			self.write(fullReplay)
 			self.add_header("Content-type", "application/octet-stream")
 			self.set_header("Content-length", len(fullReplay))
